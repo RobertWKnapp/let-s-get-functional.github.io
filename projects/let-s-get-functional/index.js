2aa911dda23ce87a392f0edf8df143ca3a7ccfaa
RobertWKnapp <4609saratoga@gmail.com>
2024-10-02 19:14:42 -0500

    33 passing

--- projects/let-s-get-functional/index.js
+++ projects/let-s-get-functional/index.js
@@ -41,48 +41,64 @@ return males.length;
 // - **Output**: `Number`
 // - **Constraints**: use `reduce`
 
-_.reduce = function(array, func, seed) {
-    let output;
-    // determine if see was not provided a value
-if (seed === undefined) {
-        output = array[0]; //  assign output the first item in the array
-        for (let i = 1; i < array.length; i++){
-        // reassign output to the result invoking the callback function
-    output = func(output, array[i], i)
-}
-        //result = func(output, array[i], i)
-  }  else { // else there was a seed value provided
-    output = seed; // assign result seed, 
-    for (let i = 0; i < array.length; i++) {
-        output = func(output, array[i], i);
-    }
-    }   
-    return output;
+// _.reduce = function(array, func, seed) {
+//     let output;
+//     // determine if seed was not provided a value
+// if (seed === undefined) {
+//         output = array[0]; //  assign output the first item in the array
+//         for (let i = 1; i < array.length; i++){
+//         // reassign output to the result invoking the callback function
+//     output = func(output, array[i], i)
+// }
+//         //result = func(output, array[i], i)
+//   }  else { // else there was a seed value provided
+//     output = seed; // assign result seed, 
+//     for (let i = 0; i < array.length; i++) {
+//         output = func(output, array[i], i);
+//     }
+//     }   
+//     return output;
 
- };
-var femaleCount = function(array) {
-    return array_.reduce((count, customer))
+//  };
+ var femaleCount = function(array) {
+    // return array_.reduce((count, customer))
     // let females = _.reduce(array, function(acc, current){
     //     // checking if current gender is female
     //     if (current.gender === 'female') {
     //         acc + 1;
     //     }
     //     return acc;
-    }, 0 );
+    // }, 0 );
+    return _.reduce(array, function(count, customer) {
+        if (customer.gender === 'female') {
+            return count + 1;
+        } else {
+            return count;
+        }
+    }, 0);  // seed is 0
 
 }
 
+// ### 3: `oldestCustomer`
+//  - **Objective**: Find the oldest customer's name
+//  - **Input**: `Array`
+//  - **Output**: `String`
+//  - **Constraints**: 
 
-var oldestCustomer = function(array) {
+ var oldestCustomer = function(array) {
+    // use reduce to find the oldest customer
     let oldest = _.reduce(array, function(accumulator, current) {
-        // accumulator = {Adele Mullin} | current = {Olga Newton}
+         // accumulator = {Adele Mullin} | current = {Olga Newton}
+        // if current customer's age is greater than the accumulator customer's age, return current
         if (current.age > accumulator.age){
-            return current;
-        }
-        return accumulator;
-    });
+           return current;
+       }  //if current is not greater than accumulator then return accumulator
+       return accumulator;
+    }); // return the name of the oldest customer.
     return oldest.name;
-};
+ };
+
+
 // invoke _.reduce()
 // output;
 // if (undefined === undefined) // true
@@ -91,19 +107,122 @@ var oldestCustomer = function(array) {
 // i = 1
 // output = func(Adele Mullin}, {Olga Newton})
 
-var youngestCustomer;
+var youngestCustomer = function(array) {
+    let youngest = _.reduce(array, function(accumulator, current) {
+        if (current.age < accumulator.age) {
+            return current; 
+        }
+        return accumulator; 
+    });
+    return youngest.name;
+}
 
-var averageBalance;
+var averageBalance = function(array) {
+    var totalBalance = _.reduce(array, function(sum, customer) {
+        var balance = Number(customer.balance.replace(/[$,]/g, ''));
+        newBalance = balance - 0;
+        return sum + newBalance;
+    }, 0);
+    return balance / array.length;
+}
 
-var firstLetterCount;
+var firstLetterCount = function(customers, letter) {
+    let lowerCaseLetter = letter.toLowerCase();
+    let customerFilter = customers.filter(function (customer){
+        return customer.name.charAt(0).toLowerCase() ===lowerCaseLetter;
+    });
+    return customerFilter.length
+}
+// ### 7: `friendFirstLetterCount`
+//  - **Objective**: Find how many friends of a given customer have names that start with a given letter
+//  - **Input**: `Array`, `Customer`, `Letter`
+//  - **Output**: `Number`
+//  - **Constraints**:
+var friendFirstLetterCount = function(customers, customerId, letter){
+    let customer = customers.find(function (cust) {
+        return cust._id === customerId;
+    });  // if the customer is not found return zero
+    if (!customer) {
+        return 0;
+    } // convert the letter to lowercase for comparison = not sure if necessary
+let lowerCaseLetter = letter.toLowerCase();
+let filteredFriends = customer.friends.filter(function(friend) {
+    return friend.name.charAt(0).toLowerCase() === lowerCaseLetter;
+});  // return the number count
+return filteredFriends.length;
+}
 
-var friendFirstLetterCount;
+// ### 8: `friendsCount`
+//  - **Objective**: Find the customers' names that have a given customer's name in their friends list
+//  - **Input**: `Array`, `Name`
+//  - **Output**: `Array`
+//  - **Constraints**:
+var friendsCount = function(customers, name) {
+  // Filter customers based on the names in their friend's list
+  const matchingCustomers = customers.filter(function (customer) {
+    return customer.friends.some(function (friend) {
+      return friend.name === name;
+    });
+  });
 
-var friendsCount;
+  // Return an array of matching customer names
+  return matchingCustomers.map(function (customer) {
+    return customer.name; // Change this to customer._id if you prefer IDs
+  });
+}
 
-var topThreeTags;
+// ### 9: `topThreeTags`
+//  - **Objective**: Find the three most common tags among all customers' associated tags
+//  - **Input**: `Array`
+//  - **Output**: `Array`
+//  - **Constraints**:
+var topThreeTags = function(customers) {
+    let tagCount = {};
+    customers.forEach(function(customer) {
+        customer.tags.forEach(function(tag) {
+            if(tagCount[tag]) {
+                tagCount[tag] = 1;
+            }
+        });
+    });
+    let tagArray = Object.entries(tagCount);
+    tagArray.sort(function (a, b) {
+        return b[1] = a[1];
+    });
+    let topThree = tagArray.slice(0, 3).map(function(tag) {
+        return tag[0];
+    });
+    return topThree;
+}
 
-var genderCount;
+// ### 10: `genderCount`
+//  - **Objective**: Create a summary of genders, the output should be:
+// ```javascript
+// {
+//     male: 3,
+//     female: 4,
+//     non-binary: 1
+// }
+// ```
+//  - **Input**: `Array`
+//  - **Output**: `Object`
+//  - **Constraints**: Use `reduce`
+var genderCount = function(customers) {
+    return customers.reduce(function (accumulator, customer) {
+        const gender = customer.gender;
+        
+        // Initialize gender count if it doesn't exist
+        if (!accumulator[gender]) {
+          accumulator[gender] = 0;
+        }
+        
+        // Increment the count for the specific gender
+        accumulator[gender]++;
+        
+        return accumulator;
+      }, {});
+    }
+
 
 //////////////////////////////////////////////////////////////////////
 // DON'T REMOVE THIS CODE ////////////////////////////////////////////
